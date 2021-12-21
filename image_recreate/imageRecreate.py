e8f56a326da292cd7961a4a175b5b7aca1adcd10
andrewbowen19 <atbowen@gmail.com>
2021-12-21 12:34:55 -0500

    Cleaned both scripts, ready to present

--- image_recreate/imageRecreate.py
+++ image_recreate/imageRecreate.py
@@ -17,165 +17,168 @@ from skimage.future import graph
 
 class imageRecreate():
 
-    def __init__(self, path=None, cmap='viridis', sub_figs: bool=False):
-#        print('Recreating image')
+    def __init__(self, path=None, cmap='viridis', sub_figs: bool = False):
         self.path = path
         self.cmap = cmap
         self.sub_figs = sub_figs
 
-    def read_image(self, image_path:str, as_array:bool=True, convert_to_greyscale:bool=False):
-        '''Reads in image data from a source file path
-        
+    def read_image(self, image_path: str, as_array: bool = True, convert_to_greyscale: bool = False):
+        '''
+        Reads in image data from a source file path
+
         parameters:
             image_path : str; path to image file to be read-in
-            as_array : bool, default True; if True, a numpy.ndarray is returned. If False, a Pillow.Image object is returned.
-            
+            as_array : bool, default True; if True, a np.ndarray is returned.
+                                If False, a Pillow.Image object is returned.
+
         returns:
-            image : PIL Image object or np.array (if as_array set to True) ; raw image data from source image, can be manipulated with other class methods
+            image : PIL Image object or np.array (if as_array set to True) ;
+                        raw image data from source image,
+                        can be manipulated with other class methods
         '''
-        
+
         image = None
-#        Try reading in from argument passed, if it's bad try the class attr
+        # Try reading in from argument passed, if it's bad try the class attr
         try:
             if image_path:
-                image = Image.open(image_path)#.convert("L")
+                image = Image.open(image_path)
             else:
-                image = Image.open(self.path)#.convert("L")
+                image = Image.open(self.path)
         except:
             print('Issue reading in image file. Double-check the path')
-        
+
         if convert_to_greyscale:
             image = image.convert("L")
-        
-#        Returning as array data
+
+        # Returning as array data
         if as_array:
             image = np.asarray(image)
-            
+
         return image
 
-    def entropy(self, image_path:str, savefig:bool=False):
+    def entropy(self, image_path: str, savefig: bool = False):
         '''
         Generates entropy plot of input image.
-        
+
         parameters:
             image_path : str; path to image we want to read in
             savefig : bool ; if True, figure is saved as pdf
-            
+
         returns:
-            entr_image : numpy.array object containing pixel data for the input image
+            entr_image : np.array containing pixel data for the input image
         '''
         print('Generating entropy image...')
-#        Read in image and compute image data entropy
+        # Read in image and compute image data entropy
         im_name = os.path.basename(image_path).split(".")[0]
         im = self.read_image(image_path, convert_to_greyscale=True)
         entr_img = entropy(im, disk(10))
-        
-#        Plotting 3 panels: raw image, image with basic colormap, entropy image with same colormap applied
+
+        # Plotting 3 panels: raw image, image with basic colormap,
+        # entropy image with same colormap applied
         if self.sub_figs:
-            f, (ax1, ax2, ax3) = plt.subplots(nrows=1, ncols=3, figsize=(9,6),sharey=True)
+            f, (ax1, ax2, ax3) = plt.subplots(nrows=1, ncols=3,
+                                              figsize=(9, 6), sharey=True)
             ax1.imshow(np.asarray(Image.open(image_path)))
             ax2.imshow(im, cmap=self.cmap)
             ax3.imshow(entr_img, cmap=self.cmap)
-            
-    #        Format the figure
+
+            # Format the figure
             for a in [ax1, ax2, ax3]:
                 a.set_axis_off()
             f.tight_layout(pad=2)
-            
+
             if savefig:
-                fig_path = os.path.join(".", 'plots', 'entropy', f'entropy_{im_name}_{self.cmap}.pdf')
-                f.savefig(fig_path, bbox_inches='tight', pad_inches = 1)
-                
-            # f.clf()
-        
+                fig_path = os.path.join(".", 'plots',
+                                        'entropy',
+                                        f'entropy_{im_name}_{self.cmap}.pdf')
+                f.savefig(fig_path, bbox_inches='tight', pad_inches=1)
+
         return entr_img
-        
-    def segment_image(self, image_path:str,  n, thresh, c, savefig:bool=False):
+
+    def segment_image(self, image_path: str,  n, thresh, c, savefig: bool = False):
         '''
-        Creates segmentation (impressionist-style) recreation of an image using scikit-image
-        
+        Creates segmentation (impressionist-style)
+            recreation of an image using scikit-image
+
         parameters:
             image_path : str; path to image we want to read in
             savefig : bool, default False ; if True, figure is saved as pdf
         '''
         im_name = os.path.basename(image_path).split(".")[0]
-        im = self.read_image(image_path, True)#, True)
-        
+        im = self.read_image(image_path, True)
+
         print(f'Generating RAG image..')
         print(f'N Segments: {n} | Threshold: {thresh} | Compactness {c}')
-        
-#        Generate image segmentation plot
-        labels1 = segmentation.slic(im, compactness=c, n_segments=n, start_label=1)
+
+        # Generate image segmentation plot
+        labels1 = segmentation.slic(im, compactness=c,
+                                    n_segments=n, start_label=1)
         out1 = color.label2rgb(labels1, im, kind='avg', bg_label=0)
 
-#        Create Region Adjacency Graph
+        # Create Region Adjacency Graph
         g = graph.rag_mean_color(im, labels1)
         labels2 = graph.cut_threshold(labels1, g, thresh)
         out2 = color.label2rgb(labels2, im, kind='avg', bg_label=0)
 
-#        Plot the image
+        # Plot the image
         if self.sub_figs:
             f, ax = plt.subplots(nrows=1, ncols=2, sharex=True, sharey=True,
-                                figsize=(8, 5))
+                                 figsize=(8, 5))
             ax[0].imshow(out1)
             ax[1].imshow(out2)
-            
+
             ax[0].set_axis_off()
             ax[1].set_axis_off()
-        
-#        Save the figure if desired
+
+        # Save the figure if desired
         if savefig:
-            f.savefig(os.path.join('plots','segmentation', f'segmentation_{im_name}_{n}_{thresh}_{c}.pdf'))
-        
+            filename = f'segmentation_{im_name}_{n}_{thresh}_{c}.pdf'
+            f.savefig(os.path.join('plots', 'segmentation', filename))
+
         return out2
-        
-    def create_panel(self, image_path:str, savefig:bool=False):
+
+    def create_panel(self, image_path: str, savefig: bool = False):
         '''
         Create 2x2 panel print contianing colormap, entropy, and RAG images
         '''
-        
-#        Generate images
+
+        # Generate images
         image = self.read_image(image_path)
         cmap_im = self.read_image(image_path, convert_to_greyscale=True)
         ent = self.entropy(image_path)
         rag = self.segment_image(image_path, 1000, 40, 25)
-        
-#        Plot the images
+
+        # Plot the images
         print('Creating plot...')
-        f, axs = plt.subplots(nrows=2, ncols=2, figsize = (8,6))
-        axs[0,0].imshow(image)
-        axs[0,1].imshow(cmap_im, cmap=self.cmap)
-        axs[1,0].imshow(ent, cmap=self.cmap)
-        axs[1,1].imshow(rag)
-        
+        f, axs = plt.subplots(nrows=2, ncols=2, figsize=(8, 6))
+        axs[0, 0].imshow(image)
+        axs[0, 1].imshow(cmap_im, cmap=self.cmap)
+        axs[1, 0].imshow(ent, cmap=self.cmap)
+        axs[1, 1].imshow(rag)
+
         for i in range(len(axs)):
             for j in range(len(axs)):
-                axs[i,j].set_axis_off()
+                axs[i, j].set_axis_off()
         f.tight_layout(pad=2)
-            
+
         if savefig:
             file_name = os.path.basename(image_path).split('.')[0]
             file_path = os.path.join("plots", file_name + "_panel.png")
             f.savefig(file_path, bbox_inches='tight')
-        
+
         return f, axs
 
-            
-if __name__=="__main__":
 
-#    Default input path/cmap
-    input_path = os.path.join(os.path.join('img', 'jeff-laurel-proposal.png'))
-    cmap = 'PuRd'
-    
-#    Get command line args if they are passed by the user
+if __name__ == "__main__":
+    # Get command line args if passed by the user, otherwise use default vals
     try:
         input_path = argv[1]
         cmap = argv[0]
-        
     except:
-        pass
-        
-#    Create the image
+        input_path = os.path.join(os.path.join('img',
+                                  'jeff-laurel-proposal-big.png'))
+        cmap = 'PuRd'
+
+    # Create the image
     ir = imageRecreate(cmap='PuRd')
     ir.create_panel(input_path, True)
-
--- image_recreate/jeffAndLaurel.py
+++ image_recreate/jeffAndLaurel.py
@@ -16,24 +16,23 @@ Message to the recipients:
 
 Jeff & Laurel,
 
-Merry Christmas! I really hope you guys like the images produced by this code. 
+Merry Christmas! I really hope you guys like the images produced by this code.
 The day Jeff proposed, we all felt so much joy being able to 'formally' welcome Laurel as a part of the family.
 I wish you guys all the happiness in the world as you start to go down this journey.
 
-I struggled trying to find a good way to edit the picture of y'all immediately after the proposal. 
+I struggled trying to find a good way to edit the picture of y'all immediately after the proposal.
 Jeff gave me the intel that Laurel's favorite color is pink, so that's where the colormap comes from.
 The pictures on the bottom are re-generated from a python machine-learning library.
     The left one is the entropy of the source image. This shows a lot of the edges and sharp changes in the image data.
     The right image's formal name is a Region-Adjacency Graph. This is a fancy name for saying that it merges regions of the painting with like colors
         It makes the image appear more like a painting with really blotchy brushstrokes.
 
-
 Jeff -- I'm so proud of you man. You were always someone I really looked up to, and still do.
-Laurel -- You've become like a sister to me and I'm so glad that you're officially becoming part of the family 
+Laurel -- You've become like a sister to me and I'm so glad that you're officially becoming part of the family.
     (even though let's be honest, you basically already were).
 
 No need to hang the print-out if it doesn't suit your style, I just wanted to have something to give y'all in addition to the code.
-If you want me to change anything with the images/get them re-printed I'm happy to do it! 
+If you want me to change anything with the images/get them re-printed I'm happy to do it!
 
 Love you both. Merry Christmas!!!
 """
@@ -44,22 +43,20 @@ from sys import argv
 import matplotlib.pyplot as plt
 
 
-if __name__=="__main__":
-    cmap = 'PuRd'
-    raw_image_path = os.path.join('.', 'img', 'jeff-laurel-proposal-big.png')
-    
-#    Allow user to pass command-line arguments if they want to change the colormap/input image
+if __name__ == "__main__":
+    # Allow user to pass command-line arguments if they want to change the colormap/input image
     try:
         raw_image_path = argv[1]
         cmap = argv[2]
-        
     except:
-        pass
+        cmap = 'PuRd'
+        raw_image_path = os.path.join('.', 'img',
+                                      'jeff-laurel-proposal-big.png')
 
-#    Recreate the image.
+    # Recreate the image.
     ir = imageRecreate(cmap=cmap)
     ir.create_panel(raw_image_path, True)
 
     plt.show()
-    
+
     print('Merry Christmas! Love you both.')
